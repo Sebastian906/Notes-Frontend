f3e8533be8bc71fe1fe4d539746da6751ada8e03
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-22 22:37:29 -0500

    Adición y configuración para el componente de la contraseña en el formulario de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import { Link } from 'react-router-dom';
+import PasswordInput from '../../components/Input/PasswordInput';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        if(!validateEmail(email)) {
+            setError('Por favor ingrese una dirección de correo.');
+            return;
+        }
+        if(!password) {
+            setError('Por favor ingrese una contraseña.');
+            return;
+        }
+        setError("")
+    };
+
     return (
         <>
             <Navbar/>
             <div className='flex items-center justify-center mt-28'>
                 <div className='w-96 border rounded bg-white px-7 py-10'>
-                    <form onSubmit={() => {}}>
-                        <h4 className='text-2xl mb-7'>Inicie Sesión</h4>
-                        <input type="text" placeholder='Correo' className='input-box'/>
-                        <button type='submit' className='btn-primary'>Iniciar Sesión</button>
+                    <form onSubmit={handleLogin}>
+
+                        <h4 className='text-2xl mb-7'>
+                            Inicie Sesión
+                        </h4>
+                        
+                        <input 
+                            type="text" 
+                            placeholder='Correo' 
+                            className='input-box'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+
+                        <PasswordInput 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+
+                        {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+
+                        <button type='submit' className='btn-primary'>
+                            Iniciar Sesión
+                        </button>
+
                         <p className='text-sm text-center mt-4'>
                             ¿Aún no se ha registrado?{" "}
                             <Link to='/signup' className='font-medium text-primary underline'>
