9cf3cf743321de970fe293918e9f419fec63ad54
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-26 18:52:07 -0500

    Creación y adición de componentes y funciones para la interfaz gráfica de las notas

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
 
 const Home = () => {
     return (
-        <div>
+        <>
             <Navbar/>
-        </div>
+
+            <div className='container mx-auto'>
+                <div className='grid grid-cols-3 gap-4 mt-8'>
+                    <NoteCard 
+                        title='Video de Software' 
+                        date='27 de Agosto, 2024'
+                        content='Teoria sobre como practicar la ingenieria'
+                        tags='#Software'
+                        isPinned={true}
+                        onEdit={()=>{}}
+                        onDelete={()=>{}}
+                        onPinNote={()=>{}}
+                    />
+                </div>
+            </div>
+
+            <button className='w-16 h-16 flex items-center justify-center rounded-full bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={() => {}}>
+                <MdAdd className='text-[32px] text-white'/>
+            </button>
+        </>
     )
 }
 
