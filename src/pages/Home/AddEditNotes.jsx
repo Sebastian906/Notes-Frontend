3b25bc83ce763d9a16db00e116941d29af325bfa
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-27 12:14:40 -0500

    Componente y funciones para añadir nota

---
+++ src/pages/Home/AddEditNotes.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const AddEditNotes = () => {
+    return (
+        <div>
+            <div className='flex flex-col gap-2'>
+                <label className='input-label'>TITULO</label>
+                <input 
+                    type="text" 
+                    className='text-2xl text-slate-950 outline-none'
+                    placeholder='Ir a clase de 4 a 6'
+                />
+            </div>
+
+            <div className='flex flex-col gap-2 mt-4'>
+                <label className='input-label'>CONTENIDO</label>
+                <textarea
+                    type='text'
+                    className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
+                    placeholder='Contenido'
+                    rows={10}
+                />
+            </div>
+
+            <div className='mt-3'>
+                <label className='input-label'>ETIQUETAS</label>
+            </div>
+
+            <button className='btn-primary font-medium mt-5 p-3' onClick={() => {}}>
+                AÑADIR
+            </button>
+        </div>
+    )
+}
+
+export default AddEditNotes
