05b1dd8556a41ae80a0fbb86e10d30e46669ac40
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-30 22:22:19 -0500

    Integración de la API para obtener los usuarios registrados en la aplicación

--- src/components/Cards/ProfileInfo.jsx
+++ src/components/Cards/ProfileInfo.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { getInitials } from '../../utils/helper';
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
+
+    if (!userInfo) {
+        return <p>Cargando información del usuario...</p>;
+    }
+
     return (
         <div className='flex items-center gap-3'>
             <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>
-                {getInitials("Sebastián Salazar")}
+                {getInitials(userInfo.fullName)}
             </div>
 
             <div>
-                <p className='text-sm font-medium'>Sebastián Salazar</p>
-                <button className='text-sm text-slate-700 underline' onClick={onLogout}>
+                <p className='text-sm font-medium'>{userInfo.fullName}</p>
+                <button 
+                    className='text-sm text-slate-700 underline' 
+                    onClick={onLogout}
+                >
                     Cerrar Sesión
                 </button>
             </div>
