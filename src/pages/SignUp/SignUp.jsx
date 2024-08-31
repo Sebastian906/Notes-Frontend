dd6848a3181558514165ed9a5353e03cccdf2e34
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-30 22:39:03 -0500

    Adición de la API para el registro de usuarios

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import PasswordInput from '../../components/Input/PasswordInput';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
 
 const SignUp = () => {
 
@@ -10,6 +11,8 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+
+    const navigate = useNavigate()
 
     const handleSignUp = async (e) => {
         e.preventDefault();
@@ -30,6 +33,33 @@ const SignUp = () => {
         }
 
         setError('')
+
+        //Lamada a la API de registro
+        try {
+            const response = await axiosInstance.post("/crear-cuenta", {
+                fullName: name,
+                email: email,
+                password: password,
+            });
+
+            // Respuesta de registro exitosa
+            if(response.data && response.data.error) {
+                setError(response.data.message);
+                return
+            }
+
+            if(response.data && response.data.accessToken) {
+                localStorage.setItem("token", response.data.accessToken)
+                navigate("/dashboard")
+            }
+        } catch (error) {
+            // Llamado de error
+            if (error.response && error.response.data && error.response.data.message) {
+                setError(error.response.data.message);
+            } else {
+                setError("Algo salió mal. Por favor intente de nuevo.");
+            }
+        }
     };
 
     return (
